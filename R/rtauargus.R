92ad3a65ad0e88625060d14e39e2fef8279c51cc
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2018-07-17 09:48:59 +0200

    ajout fonction rtauargus

---
+++ R/rtauargus.R
@@ -0,0 +1,63 @@
+param_function <- function(f, list_param) {
+  # renvoie les noms des paramètres acceptés par f présents dans list_param
+  f_param_names <-
+    intersect(
+      names(formals(f)),
+      names(list_param)
+    )
+  list_param[f_param_names]
+}
+
+
+#' @export
+
+rtauargus <- function(...) {
+
+  # obligatoire :
+  ## microdata
+  ## explanatory_vars
+  ## safety_rules
+  ## suppress
+  ## ... autres parametres à passer aux fonctions du package (cf details)
+
+  ## 0. VERIFS .............................
+
+  # verif poids
+  # if (!is.null(weighted)) {
+  #   if (is.null(weight)) stop("Définir une variable de pondération")
+  # }
+
+  ## 1. MICRO_ASC_RDA  .....................
+
+  # parametres
+  param_asc_rda <- param_function(micro_asc_rda, list(...))
+
+  # appel (+ récuperation noms asc et rda)
+  input <- do.call(micro_asc_rda, param_asc_rda)
+
+  ## 2. MICRO_ARB .........................
+
+  # parametres
+  param_arb <- param_function(micro_arb, list(...))
+  param_arb$asc_filename <- input$asc_filename
+  param_arb$rda_filename <- input$rda_filename
+
+  # appel (+ récupération nom batch)
+  batch <- do.call(micro_arb, param_arb)
+
+  ## 3. RUN_TAUARGUS ......................
+
+  # parametres
+  param_run0 <- param_function(run_tauargus, list(...))
+  param_system <- param_function(system, list(...))
+  param_run <- c(param_run0, param_system)
+  param_run$arb_filename <- batch$arb_filename
+
+  # appel
+  res <- do.call(run_tauargus, param_run)
+
+  # RESULTAT .............................
+
+  res
+
+}
