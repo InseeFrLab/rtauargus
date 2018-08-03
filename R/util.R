4aec80ee7efa01f751a0264e63a5733a3fa54307
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2018-08-03 14:07:27 +0200

    ajoute df_param_defaut (servira dans micro_asc_rda)

--- R/util.R
+++ R/util.R
@@ -10,7 +10,7 @@ cite <- function(x, guillemet = "\"", ignore_vide = TRUE) {
 }
 
 # fonction transformant une liste de parametres associes a des
-# variables en un data.frame
+# variables en un data.frame (pour hrc notamment)
 
 df_param <- function(list_param) {
 
@@ -33,6 +33,48 @@ df_param <- function(list_param) {
   res
 
 }
+
+df_param_defaut <- function(varnames, param_name, valvar) {
+
+  # fonction créant un data.frame de deux colonnes :
+  # la premiere donne le nom des variables categorielles des micro-donnees
+  #   (`varnames`)
+  # la deuxieme porte le nom `param_name`
+  #   elle prend les valeurs trouvees dans `valvar`
+  #   (un element non nommé de valvar constitute une valeur par défaut
+  #   s'appliquant à toute la colonne, les exceptions sont renseignées dans les
+  #   éléments nommés)
+
+  val_nm <- names(valvar)
+  named <- if (is.null(val_nm)) rep(FALSE, length(valvar)) else val_nm != ""
+  specifique <- valvar[named]
+  defaut <- valvar[!named]
+
+  if (!length(defaut)) defaut <- NA
+
+  if (length(defaut) > 1) {
+    defaut <- defaut[1]
+    warning(
+      'plusieurs valeurs par defaut pour "', param_name, '", ',
+      'premiere valeur prise en compte ("', defaut, '")'
+    )
+  }
+
+  res <-
+    data.frame(
+      colname = varnames,
+      .v = defaut,
+      stringsAsFactors = FALSE
+    )
+  row.names(res) <- varnames
+  names(res)[2] <- param_name
+
+  res[names(specifique), 2] <- specifique
+
+  res
+
+}
+
 
 # fonction normalizePath sans warning
 normPath2 <- purrr::partial(normalizePath, mustWork = FALSE)
