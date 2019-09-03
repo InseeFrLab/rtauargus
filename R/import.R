362dd2f18cf6526ca40d2d198fa157c185f4e173
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2019-09-03 15:15:40 +0200

    utilise arb_contents() dans import()

--- R/import.R
+++ R/import.R
@@ -33,50 +33,6 @@ read_fixheader <- function(file,
 
 }
 
-#' @importFrom purrr map
-#' @importFrom purrr map_at
-#' @importFrom purrr transpose
-#' @importFrom dplyr %>%
-
-specif_decompose <- function(specif) {
-
-  ## extrait les infos contenus dans instructions <SPECIFYTABLE> d'un batch
-  ## pour par exemple les utiliser dans reimport des données
-  ## param specif : un vecteur de commandes <SPECIFYTABLE>
-
-  # menage debut fin
-  specif <- sub("^<SPECIFYTABLE> +", "", trimws(specif))
-
-  # variable comptage
-  specif <- sub("<freq>", "Freq", specif)
-
-  # separe differentes parties
-  specif <- paste0(specif, "|") # pour dernière valeur vide (cf. ?strsplit)
-  specif_split <- strsplit(specif, "|", fixed = TRUE)
-
-  # reclasse par type de variable
-  type_var <- c("explanatory", "response", "shadow", "cost")
-  res <- transpose(specif_split, type_var)
-
-  # supprime guillements et espaces (sauf dans explanatory)
-  res <-
-    res %>%
-    map_at(
-      c("response", "shadow", "cost"),
-      ~ gsub("(\"| )", "", .x)
-    )
-
-  # separe variables dans explanatory
-  res %>%
-    map_at(
-      "explanatory",
-      ~ .x %>%
-        stringr::str_match_all('[^" ]+') %>%
-        map(~ .[ , 1])
-    )
-
-}
-
 # fonction read.csv avec parametres predefinis pour lire output TA
 my_readcsv <-
   purrr::partial(
@@ -203,80 +159,42 @@ meta_import <- function(data,
 
 import <- function(arb_filename) {
 
-  # lignes du fichier dans un vecteur
-  lignes <- readLines(arb_filename)
-
-  # extraction commandes par type
-  type_commande <- c("specifytable", "safetyrule", "suppress", "writetable")
-  commandes <-
-    lapply(
-      purrr::set_names(type_commande),
-      function(t) grep(sprintf("<%s>", toupper(t)), lignes, value = TRUE)
-    )
-
-  infos_writetable <-
-    stringr::str_match(
-      commandes$writetable,
-      "\\((\\d+),([1-6]+),(.*),\"(.+)\"\\)"
-    )
-  output_types   <- infos_writetable[ , 3]
-  output_options <- infos_writetable[ , 4]
-  output_names   <- infos_writetable[ , 5]
-
-  infos_specifytable <- specif_decompose(commandes$specifytable)
+  # extrait infos arb
+  infos <- arb_contents(arb_filename)
 
   expl_resp <-
-    infos_specifytable[c("explanatory", "response")] %>%
+    infos$specifytable[c("explanatory", "response")] %>%
     transpose() %>%
     lapply(unlist, use.names = FALSE)
 
-  commandes <-
-    commandes %>%
-    map_at(
-      c("safetyrule", "suppress"),
-      ~ gsub("<.+> +", "", trimws(.x))
-    )
-
   res <- mapply(
-    output_names,
-    output_types,
+    infos$writetable$output_names,
+    infos$writetable$output_types,
     expl_resp,
     FUN = read_ta_output,
     USE.NAMES = FALSE,
     SIMPLIFY = FALSE
   )
-
   # id des tableaux
-  motif_tid <- "^// +<TABLE_ID> +\"(.*)\"$"
-  lignes_tid <- grep(motif_tid, lignes, value = TRUE)
-  if (length(lignes_tid)) {
-    t_id <- stringr::str_match(lignes_tid, motif_tid)[ , 2]
-    names(res) <- t_id
-  }
+  if (!is.null(infos$tab_id)) names(res) <- infos$tab_id
 
-  # apriori : detection fichiers et numéros de tabulation associés
+  # apriori (pas de mix possible renseigné/non-renseigné)
   apriori <- rep(NA, length(res)) # init vecteur vide
-  motif_apriori <- '^ *<APRIORI> *"(.+?)", *(\\d{1,2}),.+$'
-  lignes_apriori <- grep(motif_apriori, lignes, value = TRUE)
-  if (length(lignes_apriori)) {
-    infos_apriori <- stringr::str_match(lignes_apriori, motif_apriori)
-    num_tab <- as.integer(infos_apriori[ , 3])
-    apriori[num_tab] <- infos_apriori[ , 2]
-  }
+  if (!is.null(infos$apriori)) apriori <- infos$apriori$file
 
   # ajout metadonnees batch
   # (specif table, safetyrule, suppress, output_type, output_options)
   mapply(
     res,
-    infos_specifytable$explanatory,
-    infos_specifytable$response,
-    infos_specifytable$shadow,
-    infos_specifytable$cost,
+    infos$specifytable$explanatory,
+    infos$specifytable$response,
+    infos$specifytable$shadow,
+    infos$specifytable$cost,
     apriori,
-    commandes$safetyrule,
-    commandes$suppress,
-    output_types,
-    output_options,
+    infos$safetyrule,
+    infos$suppress,
+    infos$writetable$output_types,
+    infos$writetable$output_options,
     FUN = meta_import,
     SIMPLIFY = FALSE
   )
